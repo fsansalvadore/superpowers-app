02efe3674e51d3513a997b726611e1751a777974
Axel Abildtrup <axabildtrup@hotmail.com>
2019-11-12 16:32:35 +0100

    Started on the controller and fixed the routes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+   # t.date "start_date"
+   #  t.date "end_date"
+   #  t.bigint "superpower_id"
+   #  t.bigint "rentee_id"
+
 end
