a8efcd38dd175fde14e9b14a818a5b0e930cb6b2
Axel Abildtrup <axabildtrup@hotmail.com>
2019-11-12 13:05:44 +0100

    seed WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,15 +45,21 @@ puts "Creating superpowers..."
     superpower_attributes << {
       name: "#{Faker::Superhero.power}",
       description: Faker::Lorem.paragraph(sentence_count: 4, supplemental: false, random_sentences_to_add: 4),
-      image: superpower_images[index],
+      # image: superpower_images[index],
       price: rand(100..1000)
     }
   end
 
-  superpower_attributes.each do |attribute|
+#   url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+# article = Article.new(title: 'NES', body: "A great console")
+# article.remote_photo_url = url
+# article.save
+
+  superpower_attributes.each do |attribute, index|
     superpower = Superpower.new(attribute)
     superpower.owner = User.all.sample
     superpower.superpower_category = SuperpowerCategory.all.sample
+    superpower.remote_image_url = superpower_images[index]
     superpower.save!
   end
 puts "Finished creating superpowers..."
