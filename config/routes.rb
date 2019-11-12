6d6abb65b9dd4e15d12f625b29f7bcbd5205a264
Axel Abildtrup <axabildtrup@hotmail.com>
2019-11-12 15:45:33 +0100

    Created the create and the new actions and forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'superpowers#index'
-  resources :superpowers
 
-  get "design", to: "pages#design"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :superpowers, only: [:index, :show, :new, :create ] do
     resources :bookings, only: [:new, :create, :update]
   end
-  get '/profile', to: 'users#show', as: 'profile'
 
+  get '/profile', to: 'users#show', as: 'profile'
+  get "design", to: "pages#design"
 end
 
