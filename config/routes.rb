10005edccc3a90d71caa3ed2b1b237b98de817ef
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2019-11-14 17:24:48 +0100

    got the landing page up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'superpowers#index'
+  root to: 'pages#home'
 
   resources :superpowers, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create]
